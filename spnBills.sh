d2f4a63818d03a5b300a45bd2f62b4a8babc0325
Lance Watson <42370566+ElDubsNZ@users.noreply.github.com>
2023-03-14 22:19:18 +1300

    Updated for Parliament website redesign
    Parliament updated their website as of 2023-03-02. It no longer allows ?page=1 etc to navigate the proposed bills.
    Through help of user on Stack Overflow:
    https://stackoverflow.com/questions/75728712/scraping-urls-from-a-website-that-no-longer-paginates-via-the-url?noredirect=1#comment133592569_75728712
    
    Have redesigned code to use APIs to construct bills/pdf lists.

--- spnBills.sh
+++ spnBills.sh
@@ -1,24 +1,21 @@
-#Pages
-pages=$(lynx -nonumbers -dump https://www.parliament.nz/en/pb/bills-and-laws/proposed-members-bills/?page=1 | grep page=[0-9]$)
-printf "$pages" | sort -u > /home/waybackMachine/pages.txt
- 
-#Bills
-while read p; do
-  page="$(lynx -nonumbers -dump $p | grep proposed-members-bills/[A-Za-z0-9])"
-  bills="${bills}\n${page}"
-done </home/waybackMachine/pages.txt
-printf "$bills" | sort -u > /home/waybackMachine/bills.txt
-sed -i '/./,$!d' /home/waybackMachine/bills.txt
- 
+#JSON Request - Output URLs of bills
+curl 'https://bills.parliament.nz/api/data/search' \
+  -K /home/waybackMachine/headers.txt \
+  --data-raw '{"documentPreset":3,"keyword":null,"selectCommittee":null,"itemType":null,"itemSubType":null,"status":[],"documentTypes":[],"beforeCommittee":null,"billStage":null,"billStages":[],"billTab":null,"billId":null,"includeBillStages":null,"subject":null,"person":null,"parliament":null,"dateFrom":null,"dateTo":null,"datePeriod":null,"restrictedFrom":null,"restrictedTo":null,"terminatedReason":null,"terminatedReasons":[],"column":4,"direction":1,"pageSize":100,"page":1}' \
+  --compressed | grep -o '"id":"[A-Za-z0-9-]*"' | cut -d'"' -f4 | sort -u | sed 's|^|https://bills.parliament.nz/v/1/|' > /home/waybackMachine/bills.txt
+
 #PDFs
-while read p; do
-  pdf="$(lynx -nonumbers -dump $p | grep https://www.parliament.nz/media/)"
+while read b; do
+url=$(echo $b | sed 's|https://bills.parliament.nz/v/1/|https://bills.parliament.nz/api/data/ProposedMembersBill/|')
+pdf=$(curl $url \
+  -K /home/waybackMachine/headers.txt \
+  --compressed | grep -o '"AttachmentId":"[A-Za-z0-9-]*"' | cut -d'"' -f4 | sort -u | sed 's|^|https://bills.parliament.nz/download/ProposedMembersBill/|')
   list="${list}\n${pdf}"
 done </home/waybackMachine/bills.txt
 printf "$list" | sort -u > /home/waybackMachine/pdfs.txt
 sed -i '/./,$!d' /home/waybackMachine/pdfs.txt
- 
+
 #Run SPN
-/home/waybackMachine/spn.sh /home/waybackMachine/pages.txt
+/home/waybackMachine/spn.sh https://bills.parliament.nz/proposed-members-bills
 /home/waybackMachine/spn.sh /home/waybackMachine/bills.txt
 /home/waybackMachine/spn.sh /home/waybackMachine/pdfs.txt
