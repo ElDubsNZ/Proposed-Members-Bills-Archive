8b6a7ca2930de4b34d910ea9a5aa3f33a70d5d86
Lance Watson <42370566+ElDubsNZ@users.noreply.github.com>
2023-04-11 14:25:16 +1200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #This creates a log of all bills for future use.
 response=$(curl 'https://bills.parliament.nz/api/data/search' \
-  -K /home/eldubs/waybackMachine/headers.txt \
+  -K /home/waybackMachine/headers.txt \
   --data-raw '{"documentPreset":3,"keyword":null,"selectCommittee":null,"itemType":null,"itemSubType":null,"status":[],"documentTypes":[],"beforeCommittee":null,"billStage":null,"billStages":[],"billTab":null,"billId":null,"includeBillStages":null,"subject":null,"person":null,"parliament":null,"dateFrom":null,"dateTo":null,"datePeriod":null,"restrictedFrom":null,"restrictedTo":null,"terminatedReason":null,"terminatedReasons":[],"column":4,"direction":1,"pageSize":100,"page":1}' \
   --compressed)
 
@@ -20,15 +20,15 @@ for i in $(seq 1 $(echo "$ids" | wc -l)); do
     memberName=$(echo "$memberNames" | sed -n "${i}p" | cut -d'"' -f4)
     publicationDate=$(echo "$publicationDates" | sed -n "${i}p" | cut -d'"' -f4)
     lastModified=$(echo "$lastModifiedDates" | sed -n "${i}p" | cut -d'"' -f4)
-    echo "$id,\"$title\",\"$partyName\",\"$memberName\",$publicationDate,$lastModified" >> "/home/eldubs/waybackMachine/log/$filename"
+    echo "$id,\"$title\",\"$partyName\",\"$memberName\",$publicationDate,$lastModified" >> "/home/waybackMachine/log/$filename"
 done
 
 # Finds the names of the two newest files in the log directory
-newest=$(find /home/eldubs/waybackMachine/log -type f -printf "%T@ %p\n" | sort -nr | head -1 | awk '{print $2}')
-secondNewest=$(find /home/eldubs/waybackMachine/log -type f -printf "%T@ %p\n" | sort -nr | head -2 | tail -1 | awk '{print $2}')
+newest=$(find /home/waybackMachine/log -type f -printf "%T@ %p\n" | sort -nr | head -1 | awk '{print $2}')
+secondNewest=$(find /home/waybackMachine/log -type f -printf "%T@ %p\n" | sort -nr | head -2 | tail -1 | awk '{print $2}')
 
 # Compares the two files using 'cmp'
 if ! cmp -s "$newest" "$secondNewest"; then
   #If there is a change, runs spnBills.sh
-  /home/eldubs/waybackMachine/spnBills.sh
+  /home/waybackMachine/spnBills.sh
 fi
